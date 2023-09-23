05e369e9aaa592c2bad583aa4cbd64c1f8f72361
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2023-09-23 22:02:21 +0700

    fix freeze in config.js

--- src/config/config.js
+++ src/config/config.js
@@ -7,9 +7,6 @@ module.exports = {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT,
-        define: {
-            freezeTableName: true,
-        },
         dialectOptions:
             process.env.DB_SSL === "true"
                 ? {
